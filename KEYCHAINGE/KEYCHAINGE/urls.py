73362f336f682e8b4ed2a15cda57e31c3c926962
1hyoeun3 <gydms0103@naver.com>
2024-07-25 00:01:48 +0900

    html updates

--- KEYCHAINGE/KEYCHAINGE/urls.py
+++ KEYCHAINGE/KEYCHAINGE/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('main/blog', views.blog, name='blog'),
+    path('main/cart', views.cart, name='cart'),
+    path('main/category', views.category, name='category'),
+    path('main/checkout', views.checkout, name='checkout'),
+    path('main/confirmation', views.confirmation, name='confirmation'),
+    path('main/contact', views.contact, name='contact'),
+    path('main/elements', views.elements, name='elements'),
+    path('main/feature', views.feature, name='feature'),
+    path('main/login', views.login, name='login'),
+    path('main/single-blog', views.SingleBlog, name='SingleBlog'),
+    path('main/single-product', views.SingleProduct, name='SingleProduct'),
+    path('main/tracking', views.tracking, name='tracking'),
 ]
--- KEYCHAINGE/KEYCHAINGE/views.py
+++ KEYCHAINGE/KEYCHAINGE/views.py
@@ -3,3 +3,39 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) :
     return render(request, 'index.html')
+
+def blog(request) :
+    return render(request, 'blog.html')
+
+def cart(request) :
+    return render(request, 'cart.html')
+
+def category(request) :
+    return render(request, 'category.html')
+
+def checkout(request) :
+    return render(request, 'checkout.html')
+
+def confirmation(request) :
+    return render(request, 'confirmation.html')
+
+def contact(request) :
+    return render(request, 'contact.html')
+
+def elements(request) :
+    return render(request, 'elements.html')
+
+def feature(request) :
+    return render(request, 'feature.html')
+
+def login(request) :
+    return render(request, 'login.html')
+
+def SingleBlog(request) :
+    return render(request, 'single-blog.html')
+
+def SingleProduct(request) :
+    return render(request, 'single-product.html')
+
+def tracking(request) :
+    return render(request, 'tracking.html')
